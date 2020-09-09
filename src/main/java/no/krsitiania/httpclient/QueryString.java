2b11dc33539a164e83e60f490a81186adf869fd8
Michael "Mike" Keenan <50305049+maikururu@users.noreply.github.com>
2020-09-09 14:45:11 +0200

    Complete parsin of QueryString (#1)
    * Created a new test QueryStringTest.shouldRetriveQueryParameter. This test is failing (still red)
    Created a new test QueryStringTest.shouldRetriveOtherQueryParameter. This test is failing (still red)
    
    *Correction, none of these tests will run as they fail to start...
    
    * Created a new test QueryStringTest.shouldRetriveQueryParameter. This test is passing (green)
    Created a new test QueryStringTest.shouldRetriveOtherQueryParameter. This test is passing (green)
    
    *Correction: none of these tests will run as they fail to start...
    *Correction 2: There were some duplications in the maven file, so it wouldnt run probably, issue is now fixed
    
    * Created another test called .shouldRetriveParameterByName (failing/red) for reading parameter name
    
    * Created another test called .shouldRetriveParameterByName() is now passing (green) for reading parameter name.
    
    Added another test shouldHandleMultipleParameters() which is failing (red) for handling multiple parameters.
    
    * Completed parsing of QueryString (4 tests passed/green)

--- src/main/java/no/krsitiania/httpclient/QueryString.java
+++ src/main/java/no/krsitiania/httpclient/QueryString.java
@@ -1,11 +1,22 @@
 package no.krsitiania.httpclient;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class QueryString {
+    private Map<String, String> parameters = new HashMap<>();
     public QueryString(String queryString){
 
+
+        for (String parameter : queryString.split("&")) {
+            int equalsPos = parameter.indexOf('=');
+            String value = parameter.substring(equalsPos + 1);
+            String parameterName = parameter.substring(0, equalsPos);
+            parameters.put(parameterName, value);
+        }
     }
 
     public String getParameter(String name) {
-        return null;
+        return parameters.get(name);
     }
 }
--- src/test/java/no/krsitiania/httpclient/QueryStringTest.java
+++ src/test/java/no/krsitiania/httpclient/QueryStringTest.java
@@ -11,5 +11,21 @@ public class QueryStringTest {
         QueryString queryString = new QueryString("status=200");
         assertEquals("200", queryString.getParameter("status"));
     }
-
+    @Test
+    void shouldOtherRetriveQueryParameter() {
+        QueryString queryString = new QueryString("status=404");
+        assertEquals("404", queryString.getParameter("status"));
+    }
+    @Test
+    void shouldRetriveParameterByName() {
+        QueryString queryString = new QueryString("text=Hello");
+        assertEquals(null, queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("text"));
+    }
+    @Test
+    void shouldHandleMultipleParameters() {
+        QueryString queryString = new QueryString("text=Hello&status=200");
+        assertEquals("200", queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("text"));
+    }
 }
