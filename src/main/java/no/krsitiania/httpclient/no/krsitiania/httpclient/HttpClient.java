d6799fea4dc2345394759a4fabcc13879cfbe853
michael <michaelkeenan@protonmail.com>
2020-09-09 15:21:01 +0200

    HTTP-client proof of concept

---
+++ src/main/java/no/krsitiania/httpclient/no/krsitiania/httpclient/HttpClient.java
@@ -0,0 +1,19 @@
+package no.krsitiania.httpclient.no.krsitiania.httpclient;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class HttpClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("urlecho.appspot.com", 80);
+
+        String request = "GET /echo?status=200&body=Hello%20world! HTTP/1.1\r\n" +
+                "Host: urlecho.appspot.com\r\n\r\n";
+        socket.getOutputStream().write(request.getBytes());
+
+        int c;
+        while((c = socket.getInputStream().read()) != -1) {
+            System.out.println((char)c);
+        }
+    }
+}
