dfc71f6286e2f85f6730e37bfba595771561f1e5
michael <michaelkeenan@protonmail.com>
2020-09-09 13:00:41 +0200

    Created a new test QueryStringTest.shouldRetriveQueryParameter. This test is failing (red)

---
+++ src/main/java/no/krsitiania/httpclient/QueryString.java
@@ -0,0 +1,11 @@
+package no.krsitiania.httpclient;
+
+public class QueryString {
+    public QueryString(String queryString){
+
+    }
+
+    public String getParameter(String name) {
+        return null;
+    }
+}
